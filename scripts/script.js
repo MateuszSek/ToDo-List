8836a3b47bf088ac2a7ec96b8b5cc520a8d4519e
Mateusz <mat.sek@wp.pl>
2018-01-23 00:46:18 +0100

    Update script.js
    changes after Maciej's advice

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,9 @@
-
 var numberOfItems = 3; //start number of items
 var doneItems=0; //to track if user did all his items
 var addItem = function(item){ 
     $("ol").prepend("<li style='font-weight:bold'>"+item.toString()+"</li>");
     $('input').val('');
     numberOfItems+=1;}   
-
-
 
 var main = function(){  
     $('#ul1').sortable(); //making it possible to prioritize tasks
@@ -20,7 +17,7 @@ var main = function(){
         }
         if (e.keyCode==46){
             $("li").each(function(){ //assigning DELETE to remove function
-                if($(this).css("text-decoration")=="line-through") {
+                if($(this).css("text-decoration")=="underline") {
                     $(this).remove();
                 };
             }); 
@@ -35,24 +32,41 @@ var main = function(){
 
     $('#deletion').on('click',function(){ //assigning remove button to remove function
         $("li").each(function(){
+            if($(this).css("text-decoration")=="underline") {
+                $(this).remove();
+                doneItems+=1;
+                if(doneItems==numberOfItems) alert('Wykonałeś wszystkie zadania!');
+            };
+        })
+    });
+
+    $('#done').on('click',function(){ //making tasks done through a buttton
+        $("li").each(function(){
+            if($(this).css("text-decoration")=="underline") {
+                $(this).css({"text-decoration":"line-through","color":"green"});
+                doneItems+=1;
+                if(doneItems==numberOfItems) alert('Wykonałeś wszystkie zadania!');
+            };
+        })
+    });
+
+
+    $('#deleteDoneTasks').on('click',function(){ //deleting done (green) tasks
+        $("li").each(function(){
             if($(this).css("text-decoration")=="line-through") {
                 $(this).remove();
             };
         })
     });
 
-    $('ol').on('dblclick','li', function(){ //crossing-out done tasks and the other way around
-        if ($(this).css("text-decoration")=="line-through") {
-            $(this).css({"text-decoration":"none","font-weight":"bold"});
-            doneItems-=1;
+    $('ol').on('dblclick','li', function(){ //choosing tasks that will be managed
+        if ($(this).css("text-decoration")=="underline") {
+            $(this).css({"text-decoration":"none","font-weight":"bold", "color":"black"});
         }
-        else {
-            $(this).css({"text-decoration":"line-through","font-weight":"normal"});
-            doneItems+=1;
-            if(doneItems==numberOfItems) alert('Wykonałeś wszystkie zadania!');
+        else if ($(this).css("text-decoration")=="none"){
+            $(this).css({"text-decoration":"underline","font-weight":"normal", "color":"red"});   
         }
     });    
 
 }
 $(document).ready(main);
-
